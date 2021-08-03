672855f38bf6064336852ca9fe6d164d9a5ae6a9
ali afraz <a.afraz.1380@gmail.com>
2021-08-03 12:38:47 +0430

    password reset implemented.

--- account/urls.py
+++ account/urls.py
@@ -1,11 +1,18 @@
+from unicodedata import name
 from django.urls import path
-from .views import CustomLoginView, UserPanelView, CustomLogoutView, Registration
+from . import views as my_view
 
 app_name = "account"
 
 urlpatterns = [
-    path('login/', CustomLoginView.as_view(), name='login'),
-    path('panel/', UserPanelView.as_view(), name='panel'),
-    path('logout/', CustomLogoutView.as_view(), name='logout'),
-    path('registration/', Registration.as_view(), name='registration'),
+    path('login/', my_view.CustomLoginView.as_view(), name='login'),
+    path('panel/', my_view.UserPanelView.as_view(), name='panel'),
+    path('logout/', my_view.CustomLogoutView.as_view(), name='logout'),
+    path('registration/', my_view.RegistrationView.as_view(), name='registration'),
+    
+    # auth app spawword reset
+    path('password-reset/', my_view.CustomPasswordResetView.as_view(), name='password_reset'),
+    path('password-reset_done/', my_view.CustomPasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('password-reset-confirm/<uidb64>/<token>/', my_view.CustomPasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('password-reset-complete/', my_view.CustomPasswordResetCompleteVeiw.as_view(), name='password_reset_complete'),
 ]
--- account/views.py
+++ account/views.py
@@ -1,9 +1,15 @@
+from django import template
 from django.shortcuts import redirect, render
 from django.contrib.auth.views import LoginView, LogoutView
+from django.urls.base import reverse
 from .forms import CustomAuthenticationForm, CustomUserCreationForm
 from django.views.generic import TemplateView, CreateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
+from django.contrib.auth.views import (PasswordResetView,
+                                        PasswordResetDoneView,
+                                        PasswordResetConfirmView,
+                                        PasswordResetCompleteView)
 
 # Create your views here.
 
@@ -26,7 +32,26 @@ class CustomLogoutView(LogoutView):
     template_name = 'logout.html'
 
 
-class Registration(CreateView):
+class RegistrationView(CreateView):
     form_class = CustomUserCreationForm
     template_name = 'registration.html'
     success_url = reverse_lazy("account:panel")
+
+
+class CustomPasswordResetView(PasswordResetView):
+    template_name = 'password_reset_form.html'
+    email_template_name = 'password_reset_email.html'
+    success_url = reverse_lazy('account:password_reset_done')
+
+
+class CustomPasswordResetDoneView(PasswordResetDoneView):
+    template_name = 'password_reset_done.html'
+
+
+class CustomPasswordResetConfirmView(PasswordResetConfirmView):
+    template_name = 'password_reset_confirm.html'
+    success_url = reverse_lazy('account:password_reset_complete')
+
+
+class CustomPasswordResetCompleteVeiw(PasswordResetCompleteView):
+    template_name = ('password_reset_complete.html')
