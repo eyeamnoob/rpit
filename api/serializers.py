fd2278d6ecb221b02c005616c28ae953bbd00838
ali afraz <a.afraz.1380@gmail.com>
2021-09-14 10:40:31 +0430

    print username instead of PK in article serializer

--- api/serializers.py
+++ api/serializers.py
@@ -3,6 +3,11 @@ from rest_framework import serializers
 
 
 class PostSerializer(serializers.ModelSerializer):
+    def get_author_username(self, obj):
+        return obj.user.username
+    
+    user = serializers.SerializerMethodField("get_author_username")
+
     class Meta:
         model = Post
-        fields = ['title', 'image', 'body', 'like', 'user', 'date']
+        fields = '__all__'
